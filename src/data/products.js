8b5620f1051c75a97cbf83f1c1de1a5ee63955f2
seligoroff <seligoroff@gmail.com>
2021-11-11 00:59:55 +0300

    products

---
+++ src/data/products.js
@@ -0,0 +1,65 @@
+export default [
+    {
+            title: "М. Вишневский, А. Алтиери. Определитель синдромов отравлений ядовитыми грибами по клинической картине",
+            price: 600,
+            image: "https://fungiline.com/wp-content/uploads/2021/09/cover_2021_1-600x942.jpg"
+    },
+    {
+            title: "Дж. Браун, Дж. Браун. В начале был гриб. Психоделические евангелия: тайная история галлюциногенов в христианстве.",
+            price: 1200,
+            image: "https://fungiline.com/wp-content/uploads/2021/06/cover_brown-600x937.jpg"
+    },
+    {
+            title: "Р. Г. Уоссон. Сома: божественный гриб бессмертия",
+            price: 2000,
+            image: "https://fungiline.com/wp-content/uploads/2021/04/soma_cover-600x943.jpg"
+    },
+    {
+            title: "Галлюциногенные грибы России. Атлас-справочник",
+            price: 2000,
+            image: "https://fungiline.com/wp-content/uploads/2020/07/ggr.jpg"
+    },
+    {
+            title: "Его Величество Мухомор",
+            price: 600,
+            image: "https://fungiline.com/wp-content/uploads/2021/06/mihail_vishnevskij_ego_velichestvo_muhomor_124527.jpg"
+    },
+    {
+            title: "Михаил Вишневский. Грибы и секс (18+)",
+            price: 1000,
+            image: "https://fungiline.com/wp-content/uploads/2020/12/cover_2020-600x894.jpg"
+    },
+    {
+            title: "Ядовитые грибы России",
+            price: 1000,
+            image: "https://fungiline.com/wp-content/uploads/2021/06/2569638_detail.jpg"
+    },
+    {
+            title: "Чага: звездный час. Современные сведения об уникальном российском грибе",
+            price: 400,
+            image: "https://fungiline.com/wp-content/uploads/2021/06/2831944_detail.jpg"
+    },
+    {
+            title: "Самые распространенные съедобные грибы: справочник-определитель начинающего грибника",
+            price: 300,
+            image: "https://fungiline.com/wp-content/uploads/2021/06/2523675_detail.jpg"
+    },
+    {
+            title: "ПОСТНАЯ ГРИБНАЯ КУХНЯ. ТРАДИЦИИ И РЕЦЕПТЫ",
+            price: 800,
+            image: "https://fungiline.com/wp-content/uploads/2021/06/postnaya-gribnaya-kuhnya-malenkaya.jpg"
+    },
+    {
+            title: "Готовим из дикоросов. Папоротники, водоросли, лишайники и грибы",
+            price: 1000,
+            image: "https://fungiline.com/wp-content/uploads/2021/06/mihail_vishnevskij_gotovim_iz_dikorosov_127645-1170x1724-1-600x884.jpg"
+    },
+    {
+            title: "Грибные заготовки: традиционные и новые рецепты",
+            price: 600,
+            image: "https://fungiline.com/wp-content/uploads/2021/06/2596646_detail.jpg"
+    }
+]
+        
+
+
