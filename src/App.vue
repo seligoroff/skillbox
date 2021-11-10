d0b81d400bd1c02e4406e347c230af5eaa1b207a
seligoroff <seligoroff@gmail.com>
2021-11-11 01:32:03 +0300

    components

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,7 @@
 <template>
   <section class="catalog">
          
-        <ul class="catalog__list">
-            
-          <li class="catalog__item" v-for="(product, index) in products" :key="index">
-            <a class="catalog__pic" href="#">
-              <img :src="product.image"  :alt="product.title">
-            </a>
-
-            <h3 class="catalog__title">
-              <a href="#">
-                {{  product.title }}
-              </a>
-            </h3>
-
-            <span class="catalog__price">
-              {{ product.price }} ₽
-            </span>
-
-            <ul class="colors colors--black">
-              <li class="colors__item">
-                <label class="colors__label">
-                  <input class="colors__radio sr-only" type="radio" name="color-1" value="#73B6EA" checked="">
-                  <span class="colors__value" style="background-color: #73B6EA;">
-                  </span>
-                </label>
-              </li>
-              <li class="colors__item">
-                <label class="colors__label">
-                  <input class="colors__radio sr-only" type="radio" name="color-1" value="#8BE000">
-                  <span class="colors__value" style="background-color: #8BE000;">
-                  </span>
-                </label>
-              </li>
-              <li class="colors__item">
-                <label class="colors__label">
-                  <input class="colors__radio sr-only" type="radio" name="color-1" value="#222">
-                  <span class="colors__value" style="background-color: #222;">
-                  </span>
-                </label>
-              </li>
-            </ul>
-          </li>
-        </ul>
+      <ProductList :products="products" />
 
         <ul class="catalog__pagination pagination">
           <li class="pagination__item">
@@ -95,12 +54,13 @@
 
 <script>
 import products from './data/products'
-for (var p in products) {
-    console.log(products[p])
-}
+import ProductList from './components/ProductList'
+
+
 
 export default {
     name: 'App',
+    components: {ProductList: ProductList},
     data() {
         return {
             products: products
