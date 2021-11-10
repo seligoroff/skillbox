2f116560af3eeb2cedef2828ff5b264a1fdff518
seligoroff <seligoroff@gmail.com>
2021-11-11 01:41:06 +0300

    components

---
+++ src/components/ColorPicker.vue
@@ -0,0 +1,31 @@
+<template>
+   <ul class="colors colors--black">
+        <li class="colors__item">
+          <label class="colors__label">
+            <input class="colors__radio sr-only" type="radio" name="color-1" value="#73B6EA" checked="">
+            <span class="colors__value" style="background-color: #73B6EA;">
+            </span>
+          </label>
+        </li>
+        <li class="colors__item">
+          <label class="colors__label">
+            <input class="colors__radio sr-only" type="radio" name="color-1" value="#8BE000">
+            <span class="colors__value" style="background-color: #8BE000;">
+            </span>
+          </label>
+        </li>
+        <li class="colors__item">
+          <label class="colors__label">
+            <input class="colors__radio sr-only" type="radio" name="color-1" value="#222">
+            <span class="colors__value" style="background-color: #222;">
+            </span>
+          </label>
+        </li>
+      </ul> 
+</template>
+
+<script>
+    export default {
+            
+    }    
+</script>    
--- src/components/ProductItem.vue
+++ src/components/ProductItem.vue
@@ -13,35 +13,16 @@
          <span class="catalog__price">
            {{ product.price }} ₽
          </span>
-
-         <ul class="colors colors--black">
-           <li class="colors__item">
-             <label class="colors__label">
-               <input class="colors__radio sr-only" type="radio" name="color-1" value="#73B6EA" checked="">
-               <span class="colors__value" style="background-color: #73B6EA;">
-               </span>
-             </label>
-           </li>
-           <li class="colors__item">
-             <label class="colors__label">
-               <input class="colors__radio sr-only" type="radio" name="color-1" value="#8BE000">
-               <span class="colors__value" style="background-color: #8BE000;">
-               </span>
-             </label>
-           </li>
-           <li class="colors__item">
-             <label class="colors__label">
-               <input class="colors__radio sr-only" type="radio" name="color-1" value="#222">
-               <span class="colors__value" style="background-color: #222;">
-               </span>
-             </label>
-           </li>
-         </ul>
+        
+        <ColorPicker />
+         
     </article>    
 </template>
     
 <script>
+    import ColorPicker from './ColorPicker'
     export default {
+         components: {ColorPicker},   
          props: ['product']   
     }
 </script>
