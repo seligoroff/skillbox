bfb1f4b245f005c360a7d3cd46a8f355043a9d73
seligoroff <seligoroff@gmail.com>
2023-04-12 11:30:58 +0300

    fix

--- src/components/ProductsFilter.vue
+++ src/components/ProductsFilter.vue
@@ -2,15 +2,15 @@
       <aside class="filter">
         <h2 class="filter__title">Фильтры</h2>
 
-        <form class="filter__form form" action="#" method="get">
+        <form class="filter__form form" action="#" method="get" @submit.prevent="submit()">
           <fieldset class="form__block">
             <legend class="form__legend">Цена</legend>
             <label class="form__label form__label--price">
-              <input class="form__input" type="text" name="min-price" v-model="currentPriceFrom">
+              <input class="form__input" type="text" name="min-price" v-model.number="currentPriceFrom">
               <span class="form__value">От</span>
             </label>
             <label class="form__label form__label--price">
-              <input class="form__input" type="text" name="max-price" v-model="currentPriceTo">
+              <input class="form__input" type="text" name="max-price" v-model.number="currentPriceTo">
               <span class="form__value">До</span>
             </label>
           </fieldset>
@@ -18,7 +18,7 @@
           <fieldset class="form__block">
             <legend class="form__legend">Категория</legend>
             <label class="form__label form__label--select">
-              <select class="form__select" type="text" name="category" v-model="currentCategoryId">
+              <select class="form__select" type="text" name="category" v-model.number="currentCategoryId">
                 <option value="0">Все категории</option>
                 <option v-for="category in categories" :value="category.id" :key="category.id">{{ category.title }}</option>                
               </select>
@@ -38,71 +38,10 @@
             
             </ul>
           </fieldset>
-<!--
-          <fieldset class="form__block">
-            <legend class="form__legend">Объемб в ГБ</legend>
-            <ul class="check-list">
-              <li class="check-list__item">
-                <label class="check-list__label">
-                  <input class="check-list__check sr-only" type="checkbox" name="volume" value="8" checked="">
-                  <span class="check-list__desc">
-                    8 
-                    <span>(313)</span>
-                  </span>
-                </label>
-              </li>
-              <li class="check-list__item">
-                <label class="check-list__label">
-                  <input class="check-list__check sr-only" type="checkbox" name="volume" value="16">
-                  <span class="check-list__desc">
-                    16 
-                    <span>(461)</span>
-                  </span>
-                </label>
-              </li>
-              <li class="check-list__item">
-                <label class="check-list__label">
-                  <input class="check-list__check sr-only" type="checkbox" name="volume" value="32">
-                  <span class="check-list__desc">
-                    32 
-                    <span>(313)</span>
-                  </span>
-                </label>
-              </li>
-              <li class="check-list__item">
-                <label class="check-list__label">
-                  <input class="check-list__check sr-only" type="checkbox" name="volume" value="64">
-                  <span class="check-list__desc">
-                    64 
-                    <span>(313)</span>
-                  </span>
-                </label>
-              </li>
-              <li class="check-list__item">
-                <label class="check-list__label">
-                  <input class="check-list__check sr-only" type="checkbox" name="volume" value="128">
-                  <span class="check-list__desc">
-                    128 
-                    <span>(313)</span>
-                  </span>
-                </label>
-              </li>
-              <li class="check-list__item">
-                <label class="check-list__label">
-                  <input class="check-list__check sr-only" type="checkbox" name="volume" value="264">
-                  <span class="check-list__desc">
-                    264 
-                    <span>(313)</span>
-                  </span>
-                </label>
-              </li>
-            </ul>
-          </fieldset>
--->
-          <button class="filter__submit button button--primery" type="submit">
+          <button class="filter__submit button button--primery" type="submit" >
             Применить
           </button>
-          <button class="filter__reset button button--second" type="button" @click="drop">
+          <button class="filter__reset button button--second" type="button" @click.prevent="reset">
             Сбросить
           </button>
         </form>
@@ -115,14 +54,41 @@
     
     export default {
           props:['page', 'priceFrom', 'priceTo', 'categoryId', 'color'],  
+          data() {
+              return {
+                  currentColor: '',
+                  currentPriceFrom: 0,
+                  currentPriceTo: 0,
+                  currentCategoryId: 0
+              }
+          },
           methods: {
-            drop() {
-                this.currentColor = '';
-                this.currentPriceFrom = '';
-                this.currentPriceTo = '';
-                this.currentCategoryId = 0;
-                this.currentPage = 1;
-            }  
+            reset() {
+                this.$emit('update:color', '');
+                this.$emit('update:priceFrom', 0);
+                this.$emit('update:priceTo', 0);
+                this.$emit('update:categoryId', 0);                
+            },
+            submit() {
+                this.$emit('update:color', this.currentColor);
+                this.$emit('update:priceFrom', this.currentPriceFrom);
+                this.$emit('update:priceTo', this.currentPriceTo);
+                this.$emit('update:categoryId', this.currentCategoryId);                
+            }
+          },
+          watch: {
+              priceFrom(value) {
+                  this.currentPriceFrom = value;
+              },
+              priceTo(value) {
+                  this.currentPriceTo = value;
+              },
+              categoryId(value) {
+                  this.currentCategoryId = value;
+              },
+              color(value) {
+                  this.currentColor = value;
+              }
           },
           computed: {
               currentPage: {
@@ -131,38 +97,6 @@
                   },
                   set(value) {
                       this.$emit('update:page', value)
-                  }
-              },
-              currentColor: {
-                    get() {
-                        return this.color
-                    },
-                    set(value) {
-                        this.$emit('update:color', value)
-                    }
-              },
-              currentPriceFrom: {
-                  get() {
-                      return this.priceFrom
-                  },
-                  set(value) {                      
-                      this.$emit('update:priceFrom', value)
-                  }
-              },
-              currentPriceTo: {
-                  get() {
-                      return this.priceTo
-                  },
-                  set(value) {                      
-                      this.$emit('update:priceTo', value)
-                  }
-              },
-              currentCategoryId: {
-                  get() {
-                      return this.categoryId
-                  },
-                  set(value) {                     
-                      this.$emit('update:categoryId', value)
                   }
               },
               categories() {
